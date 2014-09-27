aed7e1b4d7afcd78eb2554020f0c561e3db55e9c
Sean Loper <Grimmdev@gmail.com>
2014-09-27 17:18:50 -0400

    Initial Release!

---
+++ Source/Code/Test.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnGUI(){
+		if (GUILayout.Button ("Shake")) {
+			StartCoroutine(CameraShake.Shake(this.transform));
+		}
+
+		if (GUILayout.Button ("Shake")) {
+			StartCoroutine(CameraShake.Shake(this.transform,0.15f));
+		}
+	}
+}
